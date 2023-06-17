80aed1c83a2a40962285398cce43d8a5ff84ffff
Roman Artemiev <rartemiev695@gmail.com>
2023-06-17 12:35:52 +0200

    Main layout and default function for define user file

--- src/App.js
+++ src/App.js
@@ -1,43 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { CSSTransition } from 'react-transition-group';
 
 import './Styles/App.css';
-import './Styles/ResetStyles.css'
+import './Styles/ResetStyles.css';
 
+import PopUp from './Components/PopUp/PopUp';
 
 function App() {
-    return (
-        <div className="App">
-            <div class="wrapper">
-        <div class="file__container">
-            <h2 class="title">
-                To determine the format, select a file or drag it here and wait for the page to load.
-            </h2>
-            <img src="./img/Png/folder-icon.png" alt="Folder-icon" class="folder-icon"/>
-            <fieldset class="file-area">
-                <label for="file">
-                    <input type="file" id="file" class="file"/>
-                </label> 
-                <label class="file-button" for="file">
-                    Choose file
-                </label> 
-            </fieldset>
+  const [show, setShow] = useState(false);
+  const [typeFile, setTypeFile] = useState('');
+  const [fileExtension, setFileExtension] = useState('');
+
+  return (
+    <div className="App">
+      <div className="wrapper">
+        <div className="file__container">
+          <h2 className="title">
+            To determine the format, select a file or drag it here and wait for the page to load.
+          </h2>
+          <img src="./img/Png/folder-icon.png" alt="Folder-icon" className="folder-icon" />
+          <fieldset className="file-area">
+            <label htmlFor="file">
+              <input
+                value={typeFile}
+                onChange={(event) => {
+                  setShow(!show);
+                  setTypeFile(event.target.value);
+                  const extension = event.target.value.slice(event.target.value.lastIndexOf('.') + 1);
+                  setFileExtension(extension);
+                  console.log(extension);
+                }}
+                type="file"
+                id="file"
+                className="file"
+              />
+            </label>
+            <label className="file-button" htmlFor="file">
+              Choose file
+            </label>
+          </fieldset>
         </div>
-        <div class="pop-up">
-            <div class="close__pop-up">
-                <img src="./img/Svg/Close-icon.svg" alt="Close"/>
-            </div>
-            <div class="type__container">
-                <h1 class="pop-up__title">
-                    Your file is
-                </h1>
-                <p class="postscript"></p>
-            </div>
-            
-            <p class="result"></p>
-        </div>
+        <CSSTransition classNames="alert" in={show} timeout={300} unmountOnExit>
+          <React.Fragment>
+            {show && <PopUp postscript={fileExtension} closeWindow={() => setShow(!show)} />}
+          </React.Fragment>
+        </CSSTransition>
+      </div>
     </div>
-        </div>
-    );
+  );
 }
 
 export default App;
