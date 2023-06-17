d5afead28e9c1659a51b9e23760490a1724ae254
Roman Artemiev <rartemiev695@gmail.com>
2023-06-17 09:51:07 +0200

    cleaning up the file and moving the code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import './Styles/App.css';
+import './Styles/ResetStyles.css'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    return (
+        <div className="App">
+            <div class="wrapper">
+        <div class="file__container">
+            <h2 class="title">
+                To determine the format, select a file or drag it here and wait for the page to load.
+            </h2>
+            <img src="./img/Png/folder-icon.png" alt="Folder-icon" class="folder-icon"/>
+            <fieldset class="file-area">
+                <label for="file">
+                    <input type="file" id="file" class="file"/>
+                </label> 
+                <label class="file-button" for="file">
+                    Choose file
+                </label> 
+            </fieldset>
+        </div>
+        <div class="pop-up">
+            <div class="close__pop-up">
+                <img src="./img/Svg/Close-icon.svg" alt="Close"/>
+            </div>
+            <div class="type__container">
+                <h1 class="pop-up__title">
+                    Your file is
+                </h1>
+                <p class="postscript"></p>
+            </div>
+            
+            <p class="result"></p>
+        </div>
     </div>
-  );
+        </div>
+    );
 }
 
 export default App;
